b9c3ee32a487eb10c7ba53653dc731309fbbaae0
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-04 14:54:43 +0300

    Add the class GlobalExceptionHandler
    This class is intended for to provide a stable connection with users.

---
+++ src/main/java/ru/bevz/itv/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package ru.bevz.itv.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public String handleAnyException(Exception exception, Model model) {
+
+        String error = exception.getMessage() + "Вас автоматически перенаправит на главную страницу через 5 секунд!";
+        model.addAttribute("error", error);
+
+        return "error";
+    }
+
+}
