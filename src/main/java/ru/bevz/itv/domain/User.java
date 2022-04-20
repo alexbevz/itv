c2aa7ab0047ef7fd9968fa3eed3a7b21e94a5eea
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-20 23:46:26 +0300

    Update domain

--- src/main/java/ru/bevz/itv/domain/User.java
+++ src/main/java/ru/bevz/itv/domain/User.java
@@ -1,41 +1,40 @@
 package ru.bevz.itv.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Collection;
 import java.util.Set;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "\"user\"")
 public class User implements UserDetails {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String email;
+    private Long id;
+    @NotBlank(message = "username can not be empty")
+    private String username;
+    @NotBlank(message = "password can not be empty")
     private String password;
+
+    @Transient
+    private String confirmPassword;
+
     private boolean active;
+    private String activationCode;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return getRoles();
-    }
-
-    @Override
-    public String getUsername() {
-        return getEmail();
-    }
 
     @Override
     public boolean isAccountNonExpired() {
@@ -55,5 +54,29 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return isActive();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
     }
 }
