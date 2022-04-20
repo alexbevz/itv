6c126d7fc56fe1130d29fb00bf02a966c594465f
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-20 23:48:51 +0300

    Add dto

---
+++ src/main/java/ru/bevz/itv/domain/dto/CaptchaResponseDto.java
@@ -0,0 +1,17 @@
+package ru.bevz.itv.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CaptchaResponseDto {
+    private boolean success;
+    @JsonAlias("error-codes")
+    private Set<String> errorCodes;
+}
