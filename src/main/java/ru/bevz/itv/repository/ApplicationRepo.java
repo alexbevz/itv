82dac21ae32cd22bb10342f16f7724a80990d53b
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-21 22:44:33 +0300

    Update repository ApplicationRepo
    Deleted the methods that app doesn't need.

--- src/main/java/ru/bevz/itv/repository/ApplicationRepo.java
+++ src/main/java/ru/bevz/itv/repository/ApplicationRepo.java
@@ -5,15 +5,11 @@ import ru.bevz.itv.domain.Application;
 import ru.bevz.itv.domain.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ApplicationRepo extends JpaRepository<Application, Long> {
 
-    Optional<Application> findApplicationByIdAndUser(long id, User user);
-
-    Application findApplicationByIdAndUserAndNameIsNotNull(long id, User user);
-
     List<Application> getApplicationsByUserAndNameIsNotNullOrderByDtCreation(User user);
 
-    Optional<Application> findApplicationByUserAndNameIsNull(User user);
+    Application findApplicationByUserAndNameIsNull(User user);
+
 }
