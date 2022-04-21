56b18e3eaf5da70a6cd1db3c520167d8282efd92
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-21 22:52:25 +0300

    Update util ControllerUtils
    Added a method checkCaptcha that is intended for sending request to server google and receiving response (success ot not).

--- src/main/java/ru/bevz/itv/controller/ControllerUtils.java
+++ src/main/java/ru/bevz/itv/controller/ControllerUtils.java
@@ -1,14 +1,31 @@
 package ru.bevz.itv.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.client.RestTemplate;
+import ru.bevz.itv.domain.dto.CaptchaResponseDto;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class ControllerUtils {
 
-    static Map<String, String> getErrors(BindingResult bindingResult) {
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
+    @Value("${recaptcha.secret}")
+    private String recaptchaSecret;
+
+    private final RestTemplate restTemplate;
+
+    public ControllerUtils(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Map<String, String> getErrors(BindingResult bindingResult) {
         Map<String, String> errors = new HashMap<>();
 
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
@@ -21,6 +38,14 @@ public class ControllerUtils {
         }
 
         return errors;
+    }
+
+    public boolean checkCaptcha(String captchaResponse) {
+        String url = String.format(CAPTCHA_URL, recaptchaSecret, captchaResponse);
+
+        CaptchaResponseDto response =
+                restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+        return response != null && response.isSuccess();
     }
 
 }
