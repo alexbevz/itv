a30412148417260f2a4c7fd01d130cad44a2f5ed
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-22 23:26:41 +0300

    Update repository EventRepo
    Added a method that finding events for analytics between needing date.

--- src/main/java/ru/bevz/itv/repository/EventRepo.java
+++ src/main/java/ru/bevz/itv/repository/EventRepo.java
@@ -1,8 +1,25 @@
 package ru.bevz.itv.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.bevz.itv.domain.Event;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface EventRepo extends JpaRepository<Event, Long> {
 
+    @Query(
+            value = """
+                    select ev.name, count(ev.name)
+                    from Event as ev
+                    where ev.application.id = :appId and ev.dtCreation between :from and :to
+                    group by ev.name
+                    """
+    )
+    List<Object[]> findByAppIdAndDtCreationBetweenCountGroupByName(
+            long appId,
+            LocalDateTime from,
+            LocalDateTime to
+    );
 }
