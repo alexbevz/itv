174ae3ebdfb7066764307bc242cdad7f61503c9a
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-23 19:54:16 +0300

    Update repository EventRepo
    Changed a method (deleted grouping).

--- src/main/java/ru/bevz/itv/repository/EventRepo.java
+++ src/main/java/ru/bevz/itv/repository/EventRepo.java
@@ -1,7 +1,7 @@
 package ru.bevz.itv.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import ru.bevz.itv.domain.Application;
 import ru.bevz.itv.domain.Event;
 
 import java.time.LocalDateTime;
@@ -9,17 +9,6 @@ import java.util.List;
 
 public interface EventRepo extends JpaRepository<Event, Long> {
 
-    @Query(
-            value = """
-                    select ev.name, count(ev.name)
-                    from Event as ev
-                    where ev.application.id = :appId and ev.dtCreation between :from and :to
-                    group by ev.name
-                    """
-    )
-    List<Object[]> findByAppIdAndDtCreationBetweenCountGroupByName(
-            long appId,
-            LocalDateTime from,
-            LocalDateTime to
-    );
+    List<Event> findEventsByApplicationAndDtCreationBetweenOrderByDtCreation(Application app, LocalDateTime from, LocalDateTime to);
+
 }
