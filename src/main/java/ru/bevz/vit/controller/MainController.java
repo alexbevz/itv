693fa299aceff8d7a4309cfc2d5fead8cbb165ea
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-24 23:57:32 +0300

    Update controller MainController
    Added attribute countApps to model in method main.

--- src/main/java/ru/bevz/vit/controller/MainController.java
+++ src/main/java/ru/bevz/vit/controller/MainController.java
@@ -1,24 +1,37 @@
 package ru.bevz.vit.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.bevz.vit.domain.Event;
+import ru.bevz.vit.domain.User;
+import ru.bevz.vit.service.ApplicationService;
 import ru.bevz.vit.service.EventService;
 
 @Controller
 public class MainController {
 
-    private final EventService eventService;
+    private final EventService eventServ;
 
-    public MainController(EventService applicationService) {
-        this.eventService = applicationService;
+    private final ApplicationService appServ;
+
+    public MainController(EventService applicationService, ApplicationService appServ) {
+        this.eventServ = applicationService;
+        this.appServ = appServ;
     }
 
     @GetMapping("/")
-    public String main() {
+    public String main(
+            @AuthenticationPrincipal User user,
+            Model model
+    ) {
+        if (user != null) {
+            model.addAttribute("countApp", appServ.getCountAppsByUser(user));
+        }
         return "index";
     }
 
@@ -32,7 +45,7 @@ public class MainController {
     @PostMapping("/event")
     public String addEvent(@ModelAttribute("event") Event event) {
 
-        eventService.addEvent(event);
+        eventServ.addEvent(event);
 
         return "redirect:/event";
     }
