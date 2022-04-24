800afe77f30db564c61dc84825ef6cf156899d9c
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-24 23:55:35 +0300

    Update repository ApplicationRepo
    Added a method.

--- src/main/java/ru/bevz/vit/repository/ApplicationRepo.java
+++ src/main/java/ru/bevz/vit/repository/ApplicationRepo.java
@@ -5,11 +5,14 @@ import ru.bevz.vit.domain.Application;
 import ru.bevz.vit.domain.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationRepo extends JpaRepository<Application, Long> {
 
     List<Application> getApplicationsByUserAndNameIsNotNullOrderByDtCreation(User user);
 
     Application findApplicationByUserAndNameIsNull(User user);
+
+    Optional<Long> countApplicationsByUserAndNameIsNotNull(User user);
 
 }
