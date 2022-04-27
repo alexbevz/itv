54dc4f59fbd21b7057396998e197e0ebb5a56209
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-27 18:09:15 +0300

    Update controller MainController
    Added validation for events.

--- src/main/java/ru/bevz/vit/controller/MainController.java
+++ src/main/java/ru/bevz/vit/controller/MainController.java
@@ -4,13 +4,18 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.bevz.vit.domain.Application;
 import ru.bevz.vit.domain.Event;
 import ru.bevz.vit.domain.User;
 import ru.bevz.vit.service.ApplicationService;
 import ru.bevz.vit.service.EventService;
+
+import javax.validation.Valid;
+import java.util.regex.Pattern;
 
 @Controller
 public class MainController {
@@ -19,16 +24,16 @@ public class MainController {
 
     private final ApplicationService appServ;
 
-    public MainController(EventService applicationService, ApplicationService appServ) {
+    private final ControllerUtils controllerUtils;
+
+    public MainController(EventService applicationService, ApplicationService appServ, ControllerUtils controllerUtils) {
         this.eventServ = applicationService;
         this.appServ = appServ;
+        this.controllerUtils = controllerUtils;
     }
 
     @GetMapping("/")
-    public String main(
-            @AuthenticationPrincipal User user,
-            Model model
-    ) {
+    public String main(@AuthenticationPrincipal User user, Model model) {
         if (user != null) {
             model.addAttribute("countApp", appServ.getCountAppsByUser(user));
         }
@@ -43,11 +48,31 @@ public class MainController {
 
     @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/event")
-    public String addEvent(@ModelAttribute("event") Event event) {
+    public String addEvent(@RequestParam String appId, @Valid Event event, BindingResult bindingResult, Model model) {
 
-        eventServ.addEvent(event);
+        if (!Pattern.matches("^[1-9]\\d*$", appId)) {
+            bindingResult.rejectValue("application", "applicationError", "идентификатор должен быть положительным целым числом");
+        } else {
+            if (!appServ.existsAppById(Long.parseLong(appId))) {
+                bindingResult.rejectValue("application", "applicationError", "данное приложение не существует");
+            }
+        }
 
-        return "redirect:/event";
+
+        if (bindingResult.hasErrors()) {
+            model.mergeAttributes(controllerUtils.getErrors(bindingResult));
+            model.addAttribute("event", event);
+        } else {
+            Application app = new Application();
+            app.setId(Long.parseLong(appId));
+            event.setApplication(app);
+            eventServ.addEvent(event);
+            model.addAttribute("success", "событие успешно добавлено");
+        }
+
+        model.addAttribute("appId", appId);
+
+        return "event";
     }
 
 }
