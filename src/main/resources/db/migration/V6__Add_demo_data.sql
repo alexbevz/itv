b0eac0d75a95ca5ca4ef7cc7b1354c7dd36cba20
Bevz Aleksandr <as-bivz@yandex.ru>
2022-05-30 21:29:36 +0300

    Add migration V6__Add_demo_data.sql
    Added demo data.

---
+++ src/main/resources/db/migration/V6__Add_demo_data.sql
@@ -0,0 +1,22 @@
+insert into application values (1, null, 'demoApp', 1);
+insert into event
+values
+    (1, 'Some description', '2022-03-30 04:05:06', 'Login', 1),
+    (2, 'Some description', '2022-02-28 04:05:06', 'Registration', 1),
+    (3, 'Some description', '2022-01-30 04:05:06', 'Order', 1),
+    (4, 'Some description', '2022-06-04 04:05:06', 'CancelOrder', 1),
+    (5, 'Some description', '2022-06-05 04:05:06', 'Payment', 1),
+    (6, 'Some description', '2022-05-29 04:05:06', 'Login', 1),
+    (7, 'Some description', '2022-05-30 04:05:06', 'Payment', 1),
+    (8, 'Some description', '2021-12-30 04:05:06', 'Order', 1),
+    (9, 'Some description', '2021-11-30 04:05:06', 'Registration', 1),
+    (10, 'Some description', '2021-10-30 04:05:06', 'Payment', 1),
+    (11, 'Some description', '2022-05-03 04:05:06', 'Login', 1),
+    (12, 'Some description', '2022-05-02 04:05:06', 'Payment', 1),
+    (13, 'Some description', '2022-05-01 04:05:06', 'Order', 1),
+    (14, 'Some description', '2022-05-01 05:05:06', 'CancelOrder', 1),
+    (15, 'Some description', '2022-05-30 04:05:06', 'Order', 1),
+    (16, 'Some description', '2022-05-27 04:05:06', 'Login', 1),
+    (17, 'Some description', '2022-05-25 04:05:06', 'Registration', 1),
+    (18, 'Some description', '2022-05-26 04:05:06', 'Payment', 1),
+    (19, 'Some description', '2018-05-30 04:05:06', 'Payment', 1);
